72200401f18488c8284bcff9444f5e83a48373cf
Nestor Montenegro <104476402+nestord23@users.noreply.github.com>
2023-01-26 18:45:05 -0600

    Delete cinco.js

--- cinco.js
+++
@@ -1,9 +0,0 @@
-let altura =2 , radio=3;
-let area,volumen;
-console.log("ingrese el radio ");
-radio;
-console.log("ingrese el altura del cilindro");
-altura;
-area= 3.1416*radio*radio*altura;
-volumen= 2*3.1416*radio*altura;
-console.log("el volumen es de ", area , "la superficie es de", volumen);

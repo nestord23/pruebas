67a33acc7850c9dcad1e8297c7c5d4b9505ff419
Nestor Montenegro <104476402+nestord23@users.noreply.github.com>
2023-01-26 18:44:59 -0600

    Delete area.js

--- area.js
+++
@@ -1,8 +0,0 @@
-let area , base , altura  ;
-console.log("ejerccio 2");
-console.log("ingrese la base del triangulo");
-console.log(base);
-console.log("ingrese la altura del triangulo")
-console.log(altura);
-area= (base * altura)/ 2;
-console.log("la area del triangulo es de " , area);
